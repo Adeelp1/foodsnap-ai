31f83eb22bf95d51c17577e525e3fdaeca2e627b
adeel <adeelpv360@gmail.com>
2025-07-02 19:36:47 +0530

    perf: improve perfomence

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-import sys
+import os
 import food
 from nutrition import Nutrition
 
 
 if __name__ == "__main__":
-    image = sys.argv[1]
+    print("type exit for exit")
+    while True:
+        path = input("Enter image path: ")
+        if path == "exit":
+            break
+        if os.path.isfile(path):
+            name = food.find_food(path)
+            ntn = Nutrition()
+            nutri = ntn.get_nutrition(name)
+            cal, fat, carb, protien = nutri
 
-    name = food.find_food(image)
-    obj = Nutrition()
-    nutri = obj.get_nutrition(name)
-    cal, fat, carb, protien = nutri
-
-    print(f"food is a {name}\nnutritions : calories = {cal}, fat = {fat}g, carbs = {carb}g, protien = {protien}g")
+            print(f"\nfood is a {name}\nNutrients per 100 grams : calories = {cal}, fat = {fat}g, carbs = {carb}g, protien = {protien}g\n")
+        else:
+            print("Enter a valid path")
