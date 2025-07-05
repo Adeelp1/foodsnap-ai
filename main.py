bbb923f7d0cd818fc9c45a12df6d42e4c64456f2
adeel <adeelpv360@gmail.com>
2025-07-05 19:39:01 +0530

    feat: Calculate calories based on given weight

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ if __name__ == "__main__":
     print("type exit for exit")
     while True:
         path = input("Enter image path: ")
-        if path == "exit":
-            break
-        if os.path.isfile(path):
-            name = food.find_food(path)
-            ntn = Nutrition()
-            nutri = ntn.get_nutrition(name)
-            cal, fat, carb, protien = nutri
-
-            print(f"\nfood is a {name}\nNutrients per 100 grams : calories = {cal}, fat = {fat}g, carbs = {carb}g, protien = {protien}g\n")
-        else:
+        if path == "exit": break
+        if not os.path.isfile(path):
             print("Enter a valid path")
+            continue
+        weight = float(input("Please enter the weight of the food (in grams): "))
+
+        name = food.find_food(path)
+        ntn = Nutrition()
+        nutri = ntn.get_nutrition(name, weight)
+        cal, fat, carb, protien = nutri
+
+        print(f"\nfood is a {name}\nNutrients per 100 grams : calories = {cal}, fat = {fat}g, carbs = {carb}g, protien = {protien}g\n")
--- nutrition.py
+++ nutrition.py
@@ -8,8 +8,18 @@ class Nutrition:
     def __del__(self):
         self.cursor.close()
 
-    def get_nutrition(self, name):
+    def get_nutrition(self, name, weight) -> list:
         self.cursor.execute("SELECT calories, fat, carbs, protien FROM nutrition_info WHERE food_name = ?", (name,))
         nutri = self.cursor.fetchall()
+        nutrition_info = self.calculate_calories(nutri[0], weight)
 
-        return nutri[0]
+        return nutrition_info
+    
+    def calculate_calories(self, nutri_info, weight) -> list:
+        factor = weight/100
+        nutrition_result = []
+
+        for i in range(len(nutri_info)):
+            nutrition_result.append(nutri_info[i] * factor)
+
+        return nutrition_result
